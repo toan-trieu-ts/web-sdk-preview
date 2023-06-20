15709e3ca61e5456574c0779fd67aac7441b7c52
Toan Trieu <toan.trieu@trustingsocial.com>
2023-06-20 16:37:54 +0700

    Add components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://toan-trieu-ts.github.io",
-  base: "/web-sdk-preview"
+  base: "/web-sdk-preview",
+  integrations: [svelte()]
 });
---
+++ src/components/InputKeys.svelte
@@ -0,0 +1,48 @@
+<script>
+  let keyName = "";
+  let inputAccessKey = "";
+  let inputSecretKey = "";
+  let inputApiUrl = "";
+
+  let keyInputVisible = false;
+
+  const onImportStrClick = () => {
+    keyInputVisible = !keyInputVisible;
+  };
+
+  const importKeys = (keys) => {
+    let keyStr = "";
+    if (keys?.inputType === "insertFromPaste") {
+      keyStr = keys?.target?.value;
+    }
+    if (typeof keys === "string") keyStr = keys;
+    if (!keyStr) return;
+
+    const keyParsed = keyStr.split(",");
+
+    keyName = keyParsed[0];
+    inputApiUrl = keyParsed[1];
+    inputAccessKey = keyParsed[2];
+    inputSecretKey = keyParsed[3];
+
+    keyInputVisible = !keyInputVisible;
+  };
+</script>
+
+<button on:click={onImportStrClick}>IMPORT KEYS FROM STRING</button>
+<br />
+<br />
+{#if keyInputVisible}<textarea
+    cols="40"
+    rows="5"
+    id="key-input-string"
+    on:input={importKeys}
+  />{/if}
+<div id="key-parse">
+  {#if keyName !== ""}
+    <span>Key Name: <i>{keyName}</i></span><br />
+    <span id="api-url-value">Api Url: <i>{inputApiUrl}</i></span><br />
+    <span id="access-key-value">Access Key: <i>{inputAccessKey}</i></span><br />
+    <span id="secret-value">Secret Key: <i>{inputSecretKey}</i></span><br />
+  {/if}
+</div>
